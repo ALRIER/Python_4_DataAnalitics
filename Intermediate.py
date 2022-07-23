81856c75786b6a84496fa9b0be03596c8248233b
alrier <you@example.com>
2022-07-23 16:11:23 -0500

    1

---
+++ Intermediate.py
@@ -0,0 +1,121 @@
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+# Get index of 'germany': ind_ger
+ind_ger = countries.index('germany')
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+# Add poland to europe
+europe['poland'] = 'warsaw'
+# Print europe
+print(europe)
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del(europe['australia'])
+
+print(europe)
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+# Print out the capital of France
+europe['france']
+# Create sub-dictionary data
+data = {'capital':'rome', 'population':59.83 }
+# Add data to europe under key 'italy'
+europe['italy']=data
+print (europe)
+
+#lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+import pandas as pd
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict={'country':names,'drives_right':dr,'cars_per_cap':cpc}
+# Build a DataFrame cars from my_dict: cars
+cars=pd.DataFrame(my_dict)
+print (cars)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['country','drives_right']])
+
+'''loc and iloc (1)
+With loc and iloc you can do practically any data selection operation on DataFrames you can think of. loc is label-based, which means that you have to specify rows and columns based on their row and column labels. iloc is integer index based, so you have to specify rows and columns by their integer index like you did in the previous exercise.
+
+Try out the following commands in the IPython Shell to experiment with loc and iloc to select observations. Each pair of commands here gives the same result.
+
+cars.loc['RU']
+cars.iloc[4]
+
+cars.loc[['RU']]
+cars.iloc[[4]]
+
+cars.loc[['RU', 'AUS']]
+cars.iloc[[4, 1]]
+As before, code is included that imports the cars data as a Pandas DataFrame.'''
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+
+'''loc and iloc (2)
+loc and iloc also allow u2 select both rows and columns from a DataFrame.'''
+# Print out drives_right value of Morocco
+print(cars.loc['MOR'])
+
+# Print sub-DataFrame
+print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+
+
--- matplotlib.py
+++ matplotlib.py
@@ -4,3 +4,89 @@ import matplotlib.pyplot as plt
 plt.plot(deshaun.day_of_week, deshaun.hours_worked)
 # Display Deshaun's plot
 plt.show()
+
+# Plot II, exploratory plots. 
+plt.plot(deshaun.day_of_week, deshaun.hours_worked)
+plt.plot(aditya.day_of_week, aditya.hours_worked)
+plt.plot(mengfei.day_of_week, mengfei.hours_worked)
+
+# Display all three line plots
+plt.show()
+
+
+#plot
+plt.plot(six_months.month, six_months.hours_worked)
+# Add annotation "Missing June data" at (2.5, 80)
+plt.text(2.5, 80, "Missing June data")
+# Display graph
+plt.show()
+
+# Change the color of Phoenix to `"DarkCyan"`
+plt.plot(data["Year"], data["Phoenix Police Dept"],
+   label="Phoenix", color="DarkCyan")
+# Make the Los Angeles line dotted
+plt.plot(data["Year"], data["Los Angeles Police Dept"],
+   label="Los Angeles", linestyle=':')
+# Add square markers to Philedelphia
+plt.plot(data["Year"], data["Philadelphia Police Dept"],
+   label="Philadelphia", marker='s')
+# Add a legend
+plt.legend()
+# Display the plot
+plt.show()
+
+# Change the style to fivethirtyeight
+plt.style.use("fivethirtyeight")
+# Plot lines
+plt.plot(data["Year"], data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], data["Philadelphia Police Dept"], label="Philadelphia")
+# Add a legend
+plt.legend()
+# Display the plot
+plt.show()
+
+#ggplot
+plt.style.use("ggplot")
+# Plot lines
+plt.plot(data["Year"], 
+   data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], 
+   data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], 
+   data["Philadelphia Police Dept"], label="Philadelphia")
+# Add a legend
+plt.legend()
+# Display the plot
+plt.show()
+
+#style
+plt.style.use("seaborn")
+# Plot lines
+plt.plot(data["Year"], 
+   data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], 
+   data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], 
+   data["Philadelphia Police Dept"], label="Philadelphia")
+# Add a legend
+plt.legend()
+# Display the plot
+plt.show()
+
+# 
+plt.plot(ransom.letter, ransom.frequency,
+         label="Ransom",
+         linestyle=':', color='gray')
+plt.show()
+
+# Plot each line
+plt.plot(ransom.letter, ransom.frequency,
+         label='Ransom', linestyle=':', color='gray')
+
+# X-values should be suspect1.letter
+# Y-values should be suspect1.frequency
+# Label should be "Fred Frequentist"
+plt.plot(suspect1.letter, suspect1.frequency, label='Fred Frequentist')
+plt.show()
+
